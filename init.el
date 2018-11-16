1c2fea8704caa5635b80399ac77245001a7cccd9
Pin-Ju Tien <pinju.tien@gmail.com>
2018-11-15 22:16:32 -0500

    backup current init

--- init.el
+++ init.el
@@ -1,15 +1,16 @@
 (setq inhibit-startup-message t)
 
 ;;(require 'use-package)
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+;; (require 'package)
+;; (setq package-enable-at-startup nil)
+;; (add-to-list 'package-archives
+;; 	     '("melpa" . "https://melpa.org/packages/"))
 
 
 ;;;;; ensime start
 
 ;; global variables
+
 ;;(setq
 ;; inhibit-startup-screen t
 ;; create-lockfiles nil
@@ -33,21 +34,31 @@
 (global-unset-key (kbd "C-z"))
 
 
-(require 'package)
-(setq
- package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                    ("org" . "http://orgmode.org/elpa/")
-                    ("melpa-stable" . "http://stable.melpa.org/packages/"))
+;; (require 'package)
+;; (setq
+;;  package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+;;                    ("org" . "http://orgmode.org/elpa/")
+;;                    ("melpa-stable" . "http://stable.melpa.org/packages/"))
 
-package-archive-priorities '(("melpa-stable" . 1)))
-(package-initialize)
+;; package-archive-priorities '(("melpa-stable" . 1)))
+;; (package-initialize)
 
-(when (not package-archive-contents)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
+
+;; (when (not package-archive-contents)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
+;; (require 'use-package)
 
 ;;;;; ensime end
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa2" . "http://www.mirrorservice.org/sites/melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa3" . "http://www.mirrorservice.org/sites/stable.melpa.org/packages/"))
+
 
 
 (package-initialize)
@@ -77,6 +88,28 @@ package-archive-priorities '(("melpa-stable" . 1)))
 (defalias 'list-buffers 'ibuffer) ; make ibuffer default
 
 
+;; git
+(use-package magit
+  :ensure t
+  :init
+  (progn
+  (bind-key "C-x g" 'magit-status)
+  ))
+
+  (use-package git-gutter
+  :ensure t
+  :init
+  (global-git-gutter-mode +1))
+
+  (global-set-key (kbd "M-g M-g") 'hydra-git-gutter/body)
+
+
+  (use-package git-timemachine
+  :ensure t
+  )
+
+
+
 ; ace window
 (use-package ace-window
   :ensure t
@@ -85,7 +118,7 @@ package-archive-priorities '(("melpa-stable" . 1)))
     (global-set-key [remap other-window] 'ace-window)
     (custom-set-faces
      '(aw-leading-char-face
-       ((t (:inherit ace-jump-face-foreground :height 3.0 :foreground "#30ffdf" ))))) 
+       ((t (:inherit ace-jump-face-foreground :height 3.0 :foreground "#fcfcfc" ))))) 
     ))
 
 
@@ -152,104 +185,94 @@ package-archive-priorities '(("melpa-stable" . 1)))
    'self-insert-command
    minibuffer-local-completion-map))
 
-(use-package scala-mode
-  :interpreter
-  ("scala" . scala-mode))
+;; (use-package scala-mode
+;;   :interpreter
+;;   ("scala" . scala-mode))
 
 
-(use-package ensime
-  :ensure t
-  :pin melpa-stable)
+;; (use-package ensime
+;;   :ensure t
+;;   :pin melpa-stable)
 
-;; ensime https://github.com/syl20bnr/spacemacs/issues/4746
-(setq ensime-sem-high-faces
-        '(
-           (implicitConversion nil)
-           (var . (:foreground "#ff2222"))
-           (varField . (:foreground "#ff3333"))
-           (functionCall . (:foreground "#dc9157"))
-           (object . (:foreground "#D884E3"))
-           (operator . (:foreground "#cc7832"))
-           (object . (:foreground "#6897bb" :slant italic))
-           (package . (:foreground "yellow"))
-           (deprecated . (:strike-through "#a9b7c6"))
-           (implicitParams nil)
-         )
-        ;; ensime-completion-style 'company
-        ;; ensime-sem-high-enabled-p nil ;; disable semantic highlighting
-        ensime-tooltip-hints t ;; disable type-inspecting tooltips
-        ensime-tooltip-type-hints t ;; disable typeinspecting tooltips
-)
-
-
+;; ;; ensime https://github.com/syl20bnr/spacemacs/issues/4746
+;; (setq ensime-sem-high-faces
+;;         '(
+;;            (implicitConversion nil)
+;;            (var . (:foreground "#ff2222"))
+;;            (varField . (:foreground "#ff3333"))
+;;            (functionCall . (:foreground "#dc9157"))
+;;            (object . (:foreground "#D884E3"))
+;;            (operator . (:foreground "#cc7832"))
+;;            (object . (:foreground "#6897bb" :slant italic))
+;;            (package . (:foreground "yellow"))
+;;            (deprecated . (:strike-through "#a9b7c6"))
+;;            (implicitParams nil)
+;;          )
+;;         ;; ensime-completion-style 'company
+;;         ;; ensime-sem-high-enabled-p nil ;; disable semantic highlighting
+;;         ensime-tooltip-hints t ;; disable type-inspecting tooltips
+;;         ensime-tooltip-type-hints t ;; disable typeinspecting tooltips
+;; )
 
 
-;; ensime
-(add-to-list 'exec-path "/usr/local/bin")
+
+
+;; ;; ensime
+;; (add-to-list 'exec-path "/usr/local/bin")
 
 ;; theme
 
-;; dakrone theme
-;; (package-install "dakrone-theme")
-;; (load-theme 'dakrone t)
-
+;;(package-install "dakrone-theme")
+;;(load-theme 'dakrone t)
 (load-theme 'material t)
 
 
-;; (use-package dakrone-theme
-;;   :ensure t
-;;   :config (load-theme 'dakrone t))
-
-
 ;; (use-package zenburn-theme
-;;  :ensure t
-;;  :config (load-theme 'zenburn t))
+;;   :ensure t
+;;    :config (load-theme 'zenburn t))
 
 (defalias 'ar #'align-regexp)
 
 ;; highlight
 (global-hl-line-mode t)
+;; (set-face-background 'hl-line "#1794c1")
 (set-face-background 'hl-line "#585959")
 
 ;; flashes the cursor's line when you scroll
-;; (use-package beacon
-;; :ensure t
-;; :config
-;; (beacon-mode 1)
-;; ; this color looks good for the zenburn theme but not for the one
-;; ; I'm using for the videos
-;; (setq beacon-color "#666600")
-;; )
+(use-package beacon
+:ensure t
+:config
+(beacon-mode 1)
+; this color looks good for the zenburn theme but not for the one
+; I'm using for the videos
+(setq beacon-color "#666600")
+)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0 :foreground "#30ffdf"))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0 :foreground "#fcfcfc"))))
  '(lazy-highlight ((t (:foreground "white" :background "SteelBlue")))))
 
 
 ;; marked region color
-(set-face-attribute 'region nil :background "#0087ff")
+(set-face-attribute 'region nil :background "#004cdb")
 
 
 ;(global-set-key (kbd "C-x C-g") 'dumb-jump-go)
 ;(global-set-key (kbd "C-x C-b") 'dumb-jump-back)
 
-
 (use-package dumb-jump
   :bind (("M-g o" . dumb-jump-go-other-window)
 	 ("M-g j" . dumb-jump-go)
-	 ;; ("M-g j" . dumb-jump-force-searcher)
 	 ("M-g b" . dumb-jump-back)
 	 ("M-g x" . dumb-jump-go-prefer-external)
 	 ("M-g z" . dumb-jump-go-prefer-external-other-window))
    :config (setq dumb-jump-selector 'ivy) ;; (setq dumb-jump-selector 'helm)
   :ensure)
 
-(setq dumb-jump-force-searcher 'ag)
-(setq dumb-jump-prefer-searcher 'ag)
 
 (progn
   ;; turn on highlight matching brackets when cursor is on one
@@ -267,6 +290,8 @@ package-archive-priorities '(("melpa-stable" . 1)))
 ;; for scala project;; https://github.com/jacktasia/dumb-jump#configuration
 ;; does not work!
 ;; (setq dumb-jump-default-project "~/.sbt/0.13/staging/f42aec95b698116a995a/slider/")
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -274,4 +299,4 @@ package-archive-priorities '(("melpa-stable" . 1)))
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    ("dakrone-theme" dakrone-theme zygospore ztree zenburn-theme which-key use-package try org-bullets material-theme jedi flycheck ensime elpy dumb-jump counsel color-theme beacon ace-window))))
+    (evil-magit gitconfig-mode zenburn-theme which-key virtualenv use-package try realgud org-bullets material-theme jedi flycheck ensime elpy ein dumb-jump counsel company-jedi color-theme beacon auto-org-md ag ace-window 0blayout))))
